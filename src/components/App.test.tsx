6510cff52e0f38279a320b4390c3b197d3c9e099
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-10 16:06:43 +0100

    remove skeleton files and format to 2 tab width

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from "../testUtils/testUtils";
-import App from "./App";
+// import { render, screen } from "../testUtils/testUtils";
+// import App from "./App";
 
 //An example of using react-testing-library
-describe("App component", async () => {
-    test("Should have text Hello, World on it", () => {
-        render(<App />);
-        const elem = screen.getByText("Hello, World");
-        expect(elem).toBeInTheDocument();
-    });
-});
+// describe("App component", async () => {
+//     test("Should have text Hello, World on it", () => {
+//         render(<App />);
+//         const elem = screen.getByText("Hello, World");
+//         expect(elem).toBeInTheDocument();
+//     });
+// });
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,7 @@
-import { MyComponent } from "./MyComponent";
 import "./App.css";
-import { greet } from "../greet";
 
 function App() {
-    return (
-        <div className="App">
-            <MyComponent />
-
-            {greet("World")}
-        </div>
-    );
+  return <div className="App">HELLO</div>;
 }
 
 export default App;
