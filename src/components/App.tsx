76e21bfa5f8515e0aa7096d0820d11fa4266b9d4
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-10 17:19:27 +0100

    Randomly get one word from an api

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
-  return <div className="App">HELLO</div>;
+export default function App() {
+  const [randomWord, setRandomWord] = useState<string>();
+  // State keeping track of each guess, compute "mark" each render
+
+  const getRandomWord = () => {
+    fetch("https://random-word-api.herokuapp.com/word?lang=en&length=5")
+      .then((res) => res.json())
+      .then((wordList: { 0: string }) => setRandomWord(wordList[0]));
+  };
+
+  useEffect(getRandomWord, []);
+
+  return <div className="App">{randomWord}</div>;
 }
-
-export default App;
