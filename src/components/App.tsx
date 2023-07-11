e82c0ebb9c903633604ff86912407a45d307ddb6
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-07-11 10:11:45 +0100

    Added input and started styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,11 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 export default function App() {
+  //@ts-ignore
   const [randomWord, setRandomWord] = useState<string>();
-  // State keeping track of each guess, compute "mark" each render
+  //@ts-ignore
+  const [guessList, setGuessList] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState("");
 
   const getRandomWord = () => {
     fetch("https://random-word-api.herokuapp.com/word?lang=en&length=5")
@@ -11,7 +14,32 @@ export default function App() {
       .then((wordList: { 0: string }) => setRandomWord(wordList[0]));
   };
 
+  const handleOnKeyDown = (key: string) => {
+    if (key === "Enter" && inputValue.length === 5) {
+      setGuessList((prev) => {
+        const newList = [...prev, inputValue];
+        setInputValue("");
+        return newList;
+      });
+    }
+  };
+
   useEffect(getRandomWord, []);
 
-  return <div className="App">{randomWord}</div>;
+  return (
+    <div className="App">
+      {randomWord}
+      <input
+        className="input-guess"
+        maxLength={5}
+        minLength={5}
+        onChange={(e) => setInputValue(e.target.value.toUpperCase())}
+        value={inputValue}
+        onKeyDown={(e) => handleOnKeyDown(e.key)}
+      />
+      {guessList.map((guess, index) => (
+        <p key={index}>{guess}</p>
+      ))}
+    </div>
+  );
 }
